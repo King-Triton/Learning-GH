1ef35bb4222db9bf1d1df1831101f5d88dc88258
Hayden Anderson <logicgrouptutor@gmail.com>
2022-06-07 14:03:07 -0700

    Initial Commit

---
+++ Shared/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  Shared
+//
+//  Created by Hayden Anderson on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
